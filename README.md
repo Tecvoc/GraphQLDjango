d740fb4f6ed1eccb08f263adcc44415639ff96b6
Tecvoc <cloverr@tutanota.com>
2018-07-04 23:35:49 +0530

    node setup

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # GraphQLDjango
 
 ... Simple example to show the usage of graphql with django
+
+queries
+query {
+  all: allCategories {
+    name
+  },
+  ing: allIngredients {
+    name,
+    notes,
+    category {
+      name
+    }
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+var config = {
+  mode: "development",
+  entry: './frontend/entry.js',
+
+  output: {
+    path: path.resolve(__dirname, "static"),
+    filename: 'index.js',
+  },
+
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    rules: [{
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }, {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'url-loader'
+        }, ]
+      }
+    ]
+  }
+}
+
+module.exports = config;
